65ba505fbd3228b67bbc00058b29b1270c09de27
Jules Beley <jules.beley@gmail.com>
2019-01-21 23:26:37 +0100

    Graphics for polygon and column names

--- AsVectorLDAfunction.R
+++ AsVectorLDAfunction.R
@@ -1,9 +1,16 @@
 rm(list = ls())
-X2 <- data.frame(x = rnorm(2000, mean = 6, sd = 2), class = "blue")
-X3 <- data.frame(x = rnorm(10000, mean = 15, sd = 2), class = "green")
-X <- rbind(X2, X3)
+X1 <- data.frame(t = rnorm(4000, mean = 3, sd = 3), class = "red")
+X2 <- data.frame(t = rnorm(2000, mean = 6, sd = 2), class = "blue")
+X3 <- data.frame(t = rnorm(10000, mean = 15, sd = 2), class = "green")
+X4 <- data.frame(t = rnorm(7000, mean = -2, sd = 2.5), class = "orange")
+X <- rbind(X1, X2, X3, X4)
 
 lda1 <- function(value, data) {
+  if (is.numeric(data[, 1]) == TRUE) {
+    names(data) <- c("x", "class")
+  } else {
+    names(data) <- c("class", "x")
+  }
   tab <- table(data$class)
   n_cla <- dim(tab)
   len <- length(value)
@@ -23,7 +30,8 @@ lda1 <- function(value, data) {
   var <- sum(wei)
   for (i in (1:n_cla)) {
     for (j in (1:len)) {
-      dis[j, i] <- value[j] * (mu[i] / var) - (mu[i] ^ 2) / (2 * var) + log(pi[i])
+      dis[j, i] <-
+        value[j] * (mu[i] / var) - (mu[i] ^ 2) / (2 * var) + log(pi[i])
       exp[j, i] <- exp(dis[j, i])
     }
   }
@@ -64,28 +72,38 @@ lda1 <- function(value, data) {
   subout <- subout[, order(subout[1,], decreasing = TRUE)]
   x <- seq(min, max, length = 1000)
   for (i in (1:n_cla)) {
-    y[, i] <- subout[1, i] * dnorm(x, mean = subout[2, i], sd = sqrt(var))
+    y[, i] <-
+      subout[1, i] * dnorm(x, mean = subout[2, i], sd = sqrt(var))
   }
-  colors <- heat.colors(n_cla)
+  colors <- heat.colors(n_cla, alpha = 0.7)
   plot(x,
        y[, 1],
        type = "n",
        xlab = "",
        ylab = "")
-  title("Class densities")
-  lines(x, y[, 1], lwd = 2, col = colors[2])
+  title("Predicted class densities")
+  lines(x, y[, 1], lwd = 2, col = colors[1])
   for (i in (2:n_cla)) {
     lines(x, y[, i], col = colors[i], lwd = 2)
   }
-  abline(v = value[1])
-  abline(v = value[length(value)])
-  lines(density(data$x), col = "grey")
+  lines(density(data$x), col = "grey", xlim = c(min, max))
+  if (length(value) == 1) {
+    abline(v = value, col = rgb(0.5, 0.5, 0, 0.15))
+  } else {
+    polygon(
+      x = c(value[1], value[1], value[length(value)], value[length(value)]),
+      y = c(0, 1, 1, 0),
+      col = rgb(0.5, 0.5, 0, 0.15),
+      border = NA
+    )
+  }
   for (i in (1:n_cla)) {
     text(subout[2, i], 0.9 * max(y[, i]), colnames(subout)[i])
   }
   subout <- subout[, order(subout[2,])]
   rownames(subout) <- c("priors", "averages")
-  out <- list(subout, out, var)
+  out <- list(subout, out)
   return(out)
 }
-lda1(seq(7, 12, length=10), X)
+lda1(1.2, X)
+
